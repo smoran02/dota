c3c792490ab2341ab39bae9a8a7d696bd6a1d562
Spencer Moran <smoran02@gmail.com>
2013-07-03 00:29:18 -0700

    Matches link to account_ids present in each match

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ app.get('/', function(request, response){
     response.sendfile(__dirname + '/index.html');
 });
 
+app.get('/:account_id/:match_id', function(req, response){
+    var account_id = req.params.account_id;
+    var match_id = req.params.match_id;
+
+    options = {
+        protocol: 'https:',
+        host: 'api.steampowered.com',
+        pathname: '/IDOTA2Match_570/GetMatchDetails/V001/',
+        query: {key: '0BDF16C29E3E9C541EF3AF2379797588', match_id: match_id}
+    }
+
+    var dotaUrl = url.format(options);
+    request(dotaUrl, function(err, res, body){
+        var match = JSON.parse(body);
+        response.render('match.ejs', {players: match.result.players,
+                                      match_id: match_id});
+    });
+});
+
 app.get('/:account_id', function(req, response){
     var account_id = req.params.account_id;
 
@@ -22,7 +41,8 @@ app.get('/:account_id', function(req, response){
     var dotaUrl = url.format(options);
     request(dotaUrl, function(err, res, body){
         var results = JSON.parse(body);
-        response.render('results.ejs', {matches: results.result.matches, account_id: account_id});
+        response.render('results.ejs', {matches: results.result.matches, 
+                                        account_id: account_id});
     });
 });
 
